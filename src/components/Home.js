6ac8c7515be9b4c897c646fc241a22d88d2f157a
aravindh <9003557rre@gmail.com>
2024-10-01 15:14:42 +0530

    initial

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'right' }}>
+      <h1>Microsoft Club</h1>
+      <h2>Membership Card</h2>
+      <div>
+        <h3>Exclusive Events*</h3>
+        <h3>Exposure to Industries</h3>
+        <h3>Microsoft Tools and Courses</h3>
+        <h3>Project Building Sessions</h3>
+        <button>Get Your Card Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Microsoft Club</h1>
+      <div>
+        <button>Home</button>
+        <button>MSC SIST</button>
+        <button>MSC for Student</button>
+        <button>Contact</button>
+        <button>Download</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
